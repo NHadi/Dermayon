294d386fa8818c48bae8da7c79529eb0df786380
Nurul Hadi <n.hadi1996@gmail.com>
2019-10-19 22:01:07 +0700

    Add securities [Encrypt Decrypt]

---
+++ src/Dermayon/Dermayon.Common/AppSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dermayon.Common
+{
+    public class AppSetting
+    {
+        internal static class Key
+        {
+            public static string Secret = "E546C8DF278CD5931069B522E695D4F2WongDermayu";
+        }
+       
+    }
+}
---
+++ src/Dermayon/Dermayon.Common/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dermayon.Common
+{
+    public class Global
+    {
+        public static class Method
+        {
+            public static string GET = "GET";
+            public static string POST = "POST";
+            public static string PUT = "PUT";
+            public static string DELETE = "DELETE";
+        }
+    }
+}
