267131a4d7098a3188eeddd9f2c97577f4f8d52a
Nurul Hadi <n.hadi1996@gmail.com>
2019-10-19 16:40:24 +0700

    Add MongoDb RepositoryBase

--- src/Dermayon/Dermayon.Common/DermayonBootsraper.cs
+++ src/Dermayon/Dermayon.Common/DermayonBootsraper.cs
@@ -2,7 +2,7 @@
 
 namespace Dermayon.Common
 {
-    public class Class1
+    public class DermayonBootsraper
     {
     }
 }
---
+++ src/Dermayon/Dermayon.Common/Infrastructure/Data/Repositories/MongoDb/MongoDbRepository.cs
@@ -0,0 +1,28 @@
+﻿using EnsureThat;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dermayon.Common.Infrastructure.Data.Repositories.MongoDb
+{
+    public class MongoDbRepository
+    {
+        protected readonly IMongoDatabase MongoDatabase;
+        public MongoDbRepository(MongoDbSettings settings)
+        {
+            Ensure.That(settings).IsNotNull();
+            Ensure.String.IsNotNullOrEmpty(settings.ServerConnection);
+            Ensure.String.IsNotNullOrEmpty(settings.Database);
+
+            var client = new MongoClient(settings.ServerConnection);
+
+            MongoDatabase = client.GetDatabase(settings.Database,
+                new MongoDatabaseSettings
+                {
+                    GuidRepresentation = GuidRepresentation.Standard
+                });
+        }
+    }
+}
