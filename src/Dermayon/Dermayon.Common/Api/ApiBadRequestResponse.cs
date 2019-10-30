bc680630e9723a0df0adfb550ac4d1c7f07425e5
Nurul Hadi <n.hadi1996@gmail.com>
2019-10-30 09:30:50 +0700

    Add Common DTO for API

---
+++ src/Dermayon/Dermayon.Common/Api/ApiBadRequestResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dermayon.Common.Api
+{
+    public class ApiBadRequestResponse : ApiResponse
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string> InnerException { get; }
+        public ApiBadRequestResponse(int code, string message)
+            : base(400)
+        {
+            Code = code;
+            Message = message;
+        }
+    }
+}
