c0b9ce8554c8558c8f2c2537f690ef0d37d8cc6a
Nurul Hadi <n.hadi1996@gmail.com>
2019-11-02 10:06:28 +0700

    sources image

--- src/Dermayon/samples/SocialMedia/Sample.SocialMedia.UserActivity/Startup.cs
+++ src/Dermayon/samples/SocialMedia/Sample.SocialMedia.UserActivity/Startup.cs
@@ -35,6 +35,7 @@ namespace Sample.SocialMedia.UserActivity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Init DermayonBootsraper
             services.InitDermayonBootsraper()
                     // Set Kafka Configuration
                    .InitKafka()
